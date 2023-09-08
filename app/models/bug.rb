ddeceace27843c3d7dfa67350d8627219ec81939
Ajmal Abbas <ajmalabbas@apptimumlabs.net>
2023-09-08 16:07:21 +0500

    working devise

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,6 +1,6 @@
 class Bug < ApplicationRecord
   enum status: { new: 0, started: 1, completed: 2 }
   belongs_to :project
-  belongs_to :developer
-  belongs_to :qa
+  belongs_to :developer, class_name: 'User', optional: true
+  belongs_to :qa, class_name: 'User'
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
-  belongs_to :manager
+  belongs_to :manager, class_name: 'User'
+  has_many :bugs
 end
